22405dbee5f2d13da847dba4d6dc01e23927b271
ODIN74 <fantompus@gmail.com>
2018-06-04 16:44:07 +0500

    Начата работа над классами игрока и игрового процесса

---
+++ PokerProject/Game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokerProject
+{
+    public class Game
+    {
+        private static uint roundCounter = 1;
+        private uint bank;
+        private uint bigblind;
+
+        public Game()
+        { }
+
+        public Game(uint bigblind)
+        {
+            this.bigblind = bigblind;
+            this.bank = 0;
+        }
+
+        public uint Bank
+        {
+            get { return bank; }
+        }
+        
+        public void FirstIncreaseBank(uint bet)
+        {
+            if (bet >= 2 * bigblind)
+                this.bank += bet;
+            else MessageBox.Show("Ставка не может быть меньше двух больших блайдов!", "Подсказка");
+        }
+
+        public void OtherIncreaseBank (uint currentBet, uint previousBet)
+        {
+            if (currentBet >= 2 * previousBet)
+                this.bank += currentBet;
+            else MessageBox.Show("Рейз должет быть как минимум в два раза больше предыдущей ставки!", "Подсказка");
+        }
+    }
+}
---
+++ PokerProject/Player.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerProject
+{
+    public class Player
+    {
+        private string name;
+        private uint stack = 1000;
+        private bool button;
+        private uint currentBet;
+
+        public Player()
+        { }
+
+        public Player(string name, bool button)
+        {
+            this.name = name;
+            this.button = button;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public bool Button 
+        {
+            get { return button; }
+            set { button = value; }
+        }
+
+        public uint Stack
+        {
+            get { return stack; }
+            set { stack = value; }
+        }
+
+        public uint CurrentBet
+        {
+            set { currentBet = value; }
+        }
+
+        public void IncreaseStack (uint bank)
+        {
+            this.stack += bank;
+        }
+
+        public void ReductionStack (ref uint currentBet)
+        {
+            this.stack -= currentBet;
+        }
+
+    }
+}
